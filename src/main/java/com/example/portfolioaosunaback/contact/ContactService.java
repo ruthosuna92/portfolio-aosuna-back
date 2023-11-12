4f486eb5d4bbad898a3db6d1208b2d60453437a4
Ruth <alejandraop391@gmail.com>
2023-11-11 22:07:45 -0500

    Applying JPA repository methods!

--- src/main/java/com/example/portfolioaosunaback/contact/ContactService.java
+++ src/main/java/com/example/portfolioaosunaback/contact/ContactService.java
@@ -1,21 +1,19 @@
 package com.example.portfolioaosunaback.contact;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ContactService {
+    private final ContactRepository contactRepository;
+    @Autowired
+    public ContactService(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
 
     public List<Contact> getContacts() {
-        return List.of(
-                new Contact(
-                        1L,
-                        "Demo name",
-                        "Demo Subject",
-                        "Demo@email.com",
-                        "Demo Description"
-                )
-        );
+       return contactRepository.findAll();
     }
 }
