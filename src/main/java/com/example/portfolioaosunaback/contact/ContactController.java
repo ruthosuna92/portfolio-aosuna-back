b23d45be13f8f99ba9dda6cde51d4bcabf7ae6ec
Ruth <alejandraop391@gmail.com>
2023-11-27 21:05:27 -0500

    template engine

--- src/main/java/com/example/portfolioaosunaback/contact/ContactController.java
+++ src/main/java/com/example/portfolioaosunaback/contact/ContactController.java
@@ -1,10 +1,12 @@
 package com.example.portfolioaosunaback.contact;
 
 import com.example.portfolioaosunaback.email.EmailSender;
+import com.example.portfolioaosunaback.email.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import org.thymeleaf.context.Context;
 
 @RestController
 @RequestMapping(path = "contacts")
@@ -12,10 +14,14 @@ public class ContactController {
     private final ContactService contactService;
 
     private final EmailSender emailSender;
+    private final EmailSenderService emailSenderService;
+
     @Autowired
-    public ContactController(ContactService contactService, EmailSender emailSender) {
+    public ContactController(ContactService contactService, EmailSender emailSender, EmailSenderService emailSenderService) {
         this.contactService = contactService;
         this.emailSender = emailSender;
+        this.emailSenderService = emailSenderService;
+
     }
 
     @GetMapping
@@ -24,13 +30,19 @@ public class ContactController {
     }
 
     @PostMapping
-    public ResponseEntity<String> registerNewContact(@RequestBody Contact contact) {
+    public ResponseEntity<?> registerNewContact(@RequestBody Contact contact) {
 
         contactService.addNewContact(contact);
         emailSender.sendEmail(contact.getEmail(),
                 contact.getSubject(),
                 contact.getDescription(),
                 contact.getName());
+
+        Context context = new Context();
+        context.setVariable("message", contact.getDescription());
+
+        emailSenderService.sendEmailWithHtmlTemplate(contact.getEmail(), "Thank you for your interest in Ale Developer", "email-template", context);
+
         return ResponseEntity.ok("Success");
     }
 
--- src/main/java/com/example/portfolioaosunaback/email/EmailSenderService.java
+++ src/main/java/com/example/portfolioaosunaback/email/EmailSenderService.java
@@ -1,16 +1,23 @@
 package com.example.portfolioaosunaback.email;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Service
 public class EmailSenderService implements EmailSender {
 
     private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
 
-    public EmailSenderService(JavaMailSender mailSender) {
+    public EmailSenderService(JavaMailSender mailSender, TemplateEngine templateEngine) {
         this.mailSender = mailSender;
+        this.templateEngine = templateEngine;
     }
 
     @Override
@@ -26,4 +33,17 @@ public class EmailSenderService implements EmailSender {
 
         this.mailSender.send(simpleMailMessage);
     }
-}
+
+    public void sendEmailWithHtmlTemplate(String to, String subject, String templateName, Context context) {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
+        try {
+            helper.setTo(to);
+            helper.setSubject(subject);
+            String htmlContent = templateEngine.process(templateName, context);
+            helper.setText(htmlContent, true);
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            // Handle exception
+        }
+}}
